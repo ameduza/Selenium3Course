0434ef51cb47f548a20ef710c493f96182535aaa
Alexander Maximov <ameduza@gmail.com>
2016-12-14 00:56:46 +0300

    Initial commit. Lesson2, UnitTest1

---
+++ Lesson2/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace Selenium2
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        private IWebDriver driverFf;
+        private WebDriverWait waitFF;
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            driverFf = new FirefoxDriver();
+            waitFF = new WebDriverWait(driverFf, TimeSpan.FromSeconds(11));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://www.google.com/";
+            driver.FindElement(By.Name("q")).SendKeys("webdriver");
+            driver.FindElement(By.Name("btnG")).Click();
+            wait.Until(ExpectedConditions.TitleIs("webdriver - Поиск в Google"));
+        }
+
+        [Test]
+        public void FfTest()
+        {
+            driverFf.Url = "http://www.google.com/";
+            driverFf.FindElement(By.Name("q")).SendKeys("webdriver");
+            driverFf.FindElement(By.Name("btnG")).Click();
+            waitFF.Until(ExpectedConditions.TitleIs("webdriver - Поиск в Google"));
+
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
