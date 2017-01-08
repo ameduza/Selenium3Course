0cb0f765e3081875f6abf512d58f3bef8d8cc448
Alexander Maximov <ameduza@gmail.com>
2017-01-09 01:36:37 +0300

    Lesson6, Task 11 - RegisterNewUser

--- Lesson6/Base.cs
+++ Lesson6/Base.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using OpenQA.Selenium;
 
 
 namespace Lesson6
@@ -18,6 +20,21 @@ namespace Lesson6
         protected static string Argb2RgbaFormat(Color c)
         {
             return String.Format("rgba({0}, {1}, {2}, {3})", c.R, c.G, c.B, 1);  // вместо c.A пришслось вписать 1
+        }
+
+        protected void SendKeys(IWebElement element, string keys)
+        {
+            element.Clear();
+            element.SendKeys(keys);
+        }
+
+        protected static string GetRandomString(int length)
+        {
+            var random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+
         }
     }
 }
--- Lesson6/PageObjects/MainPage.cs
+++ Lesson6/PageObjects/MainPage.cs
@@ -5,20 +5,25 @@ using OpenQA.Selenium.Support.UI;
 
 namespace Lesson6
 {
-    public class MainPage
+    public class MainPage : Base
     {
         private IWebDriver _driver;
         private WebDriverWait _wait;
-        private readonly By _productsListLocator = By.CssSelector("li.product");
-        private readonly By _productsStickerLocator = By.CssSelector("div.sticker");
-        [FindsBy(How = How.CssSelector, Using = @"div#box-campaigns a.link")]
-        private IWebElement _productsCampaigns { get; set; }
-
-        private readonly By _productNameLocator = By.CssSelector(@"div.name");
-        private readonly By _productManufacturerLocator = By.CssSelector(@"div.manufacturer");
-        private readonly By _productPriceRegularLocator = By.CssSelector(@"div.price-wrapper s.regular-price");
-        private readonly By _productPriceCampaignLocator = By.CssSelector(@"div.price-wrapper strong.campaign-price");
         
+        [FindsBy(How = How.CssSelector, Using = @"div#box-account-login tr:nth-child(5) a")]
+        private IWebElement _registerCustomerElement { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = @"div#box-account li:nth-child(4) a")]
+        private IWebElement _logoutLinkElement { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = @"div#box-account-login input[name=email]")]
+        private IWebElement _emailFieldElement { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = @"div#box-account-login input[name=password]")]
+        private IWebElement _passwordFieldElement { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = @"div#box-account-login button[name=login]")]
+        private IWebElement _loginButtonElement { get; set; }
 
         public MainPage(IWebDriver driver, WebDriverWait wait)
         {
@@ -27,46 +32,25 @@ namespace Lesson6
             PageFactory.InitElements(_driver, this);
         }
 
-        public int GetProductsCount()
+
+        public CreateAccountPage OpenCreateAccountPage()
         {
-            return _driver.FindElements(_productsListLocator).Count;
+            _registerCustomerElement.Click();
+            return new CreateAccountPage(_driver, _wait);
         }
 
-        public IList<IWebElement> GetProductsList(IWebDriver driver)
+        public MainPage DoLogout()
         {
-            return new List<IWebElement>(driver.FindElements(_productsListLocator));
+            _logoutLinkElement.Click();
+            return new MainPage(_driver, _wait);
         }
 
-        public int GetProductStickerCount(IWebElement product)
+        public MainPage DoLogin(string email, string password)
         {
-            return product.FindElements(_productsStickerLocator).Count;
-        }
-
-
-        public ProductPage OpenFirstCampaignsProduct()
-        {
-            _productsCampaigns.Click();
-            return new ProductPage(_driver, _wait);
-        }
-        public Product GetCampaignsFirstProductDetails()
-        {
-            var p = new Product
-            {
-                Name = _productsCampaigns.FindElement(_productNameLocator).GetAttribute("textContent"),
-                Manufacturer = _productsCampaigns.FindElement(_productManufacturerLocator).GetAttribute("textContent"),
-                
-                PriceRegular = _productsCampaigns.FindElement(_productPriceRegularLocator).Text,
-                PriceRegularColor = _productsCampaigns.FindElement(_productPriceRegularLocator).GetCssValue("color"),      // should be == '#777' (gray)
-                PriceRegularStyle = _productsCampaigns.FindElement(_productPriceRegularLocator).GetAttribute("tagName"),   // should be == s
-                PriceRegularSize = float.Parse(_productsCampaigns.FindElement(_productPriceRegularLocator).GetCssValue("font-size").Remove(2)), // remove 'px', 'em' etc.
-                
-                PriceCampaign = _productsCampaigns.FindElement(_productPriceCampaignLocator).Text,
-                PriceCampaignColor = _productsCampaigns.FindElement(_productPriceCampaignLocator).GetCssValue("color"),      // should be == '#c00' (red)
-                PriceCampaignStyle = _productsCampaigns.FindElement(_productPriceCampaignLocator).GetAttribute("tagName"),   // should be == strong
-                PriceCampaignSize = float.Parse(_productsCampaigns.FindElement(_productPriceCampaignLocator).GetCssValue("font-size").Remove(2)) // remove 'px', 'em' etc.
-
-            };
-            return p;
+            SendKeys(_emailFieldElement, email);
+            SendKeys(_passwordFieldElement, password);
+            _loginButtonElement.Click();
+            return new MainPage(_driver, _wait);
         }
     }
 }
